84af7d7767f27df6b8a1d1a32095a82e5d9257f3
muller <pedromullergl11@gmail.com>
2023-04-16 18:30:12 -0300

    fix fetch data

--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,8 +1,5 @@
 export const getFetch = async () => {
   const API = await fetch('https://economia.awesomeapi.com.br/json/all');
-  const data = await API.json();
-  console.log(data);
-  // delete data.USDT;
-  // delete explicado pelo Clecio da turma 29.
+  const { USDT, ...data } = await API.json();
   return data;
 };
