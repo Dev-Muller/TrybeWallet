295d9e07acaa818844878475bd21f80aacfdfcc6
Pedro Muller <pedromullergl11@gmail.com>
2023-04-17 09:59:00 -0300

    fix: retornando fetch do jeito q estava antes

--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,5 +1,8 @@
 export const getFetch = async () => {
   const API = await fetch('https://economia.awesomeapi.com.br/json/all');
-  const { USDT, ...data } = await API.json();
+  const data = await API.json();
+  // const { USDT, ...data} = await API.json();
+  delete data.USDT;
+  // delete explicado pelo Clecio turma 29
   return data;
 };
